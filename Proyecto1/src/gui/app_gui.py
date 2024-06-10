e4d91c44882e7707a6f05b7d3dbac70e211db094
edyrrg <edyrojas64@gmail.com>
2024-06-09 18:46:10 -0600

    dev dialogs and frames to mouckup views

--- Proyecto1/src/gui/app_gui.py
+++ Proyecto1/src/gui/app_gui.py
@@ -1,32 +1,84 @@
+import os
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox, font, filedialog
 
-class AppGui:
-    def __init__(self, root):
-        self.root = root
-        self.root.title('Grafos Guatemala')
+
+class AppGui(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Grafos Guatemala')
+        global_font = font.Font(family="Helvetica", size=10)
+        self.option_add("*Font", global_font)
         self.center_window(1200, 800)
-        self.root.resizable(False, False)
-        self.label = None
-        self.boton = None
-        self.create_widgets()
+        self.resizable(False, False)
+        self.file_paths = {}
+        self.frames = {}
+        self.current_frame = None
+        self.create_menu()
+        self.create_frames()
 
     def center_window(self, _width, _height):
-        width_screen = self.root.winfo_screenwidth()
-        height_screen = self.root.winfo_screenheight()
+        width_screen = self.winfo_screenwidth()
+        height_screen = self.winfo_screenheight()
 
         # x = (with_screen / 2) - (20 / 2)
         # y = (height_screen / 2) - (20 / 2)
         x = (width_screen - _width) // 2
         y = (height_screen - _height) // 2
 
-        self.root.geometry(f"{_width}x{_height}+{x}+{y}")
+        self.geometry(f"{_width}x{_height}+{x}+{y}")
 
-    def create_widgets(self):
-        # Crear un label
-        self.label = ttk.Label(self.root, text="¡Hola, mundo!")
-        self.label.pack(pady=10)
+    def create_menu(self):
+        menubar = tk.Menu(self)
+        self.config(menu=menubar)
 
-        # Crear un botón
-        self.boton = ttk.Button(self.root, text="Haz clic aquí")
-        self.boton.pack(pady=10)
+        file_menu = tk.Menu(menubar, tearoff=0)
+        report_menu = tk.Menu(menubar, tearoff=0)
+
+        menubar.add_cascade(label="Archivo", menu=file_menu)
+        menubar.add_cascade(label="Reportes", menu=report_menu)
+
+        file_menu.add_command(label="Cargar Archivo", command=lambda: self.file_handler())
+        file_menu.add_command(label="Ejecutar Archivo", command=lambda: self.show_info())
+
+        report_menu.add_command(label="Reporte de Tokens", command=lambda: self.show_info())
+        report_menu.add_command(label="Reporte de Errores", command=lambda: self.show_info())
+
+    def show_info(self):
+        messagebox.showinfo("Estado", "Funciona :D")
+
+    def create_frames(self):
+        # Crear y almacenar los frames en un diccionario
+        self.frames["ShowProcessFrame"] = tk.Frame(self)
+        self.frames["ExecFileFrame"] = ttk.Frame(self)
+        self.frames["ReportTokensFrame"] = ttk.Frame(self)
+        self.frames["ReportErrorsFrame"] = ttk.Frame(self)
+
+        # Mostrar el frame inicial
+        self.show_frame("ShowProcessFrame")
+
+    def show_frame(self, frame_name):
+        frame = self.frames[frame_name]
+        frame.pack(fill=tk.BOTH, expand=True)
+
+        if hasattr(self, 'current_frame') and self.current_frame is not None:
+            self.current_frame.pack_forget()
+
+        self.current_frame = frame
+
+    def file_handler(self):
+        path = filedialog.askopenfilename(
+            title="Seleccione un archivo",
+            initialdir="C:/",
+            filetypes=[
+                ("Archivos", "*.txt"),
+            ]
+        )
+        if path != "" and path is not None:
+            # print(path)
+            count_path_files = len(self.file_paths)
+            count_path_files += 1
+            self.file_paths[f"{count_path_files}"] = path
+            print(self.file_paths)
+        else:
+            messagebox.showerror("Error", "No se selecciono un archivo")
--- Proyecto1/src/main.py
+++ Proyecto1/src/main.py
@@ -1,11 +1,9 @@
 # This is a sample Python script.
 from src.gui.app_gui import AppGui
-import tkinter as tk
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    root = tk.Tk()
-    AppGui(root)
-    root.mainloop()
+    app = AppGui()
+    app.mainloop()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
